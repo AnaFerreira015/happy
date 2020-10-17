c7ec89c0b81287fcab3435410639e8a6ba4a3381
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2020-10-17 11:16:26 -0300

    Organiza as importações

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/components/sidebar.css';
-import { useHistory } from 'react-router-dom';
 
 export default function Sidebar() {
   const { goBack } = useHistory();
